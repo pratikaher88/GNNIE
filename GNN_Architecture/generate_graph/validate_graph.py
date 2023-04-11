0d33e92944630c571f5ba6d5142cd818889be9e7
aayushisanghi <aayu.sanghi@gmail.com>
2023-04-10 22:08:04 -0700

    makr validation generalised

--- GNN_Architecture/generate_graph/validate_graph.py
+++ GNN_Architecture/generate_graph/validate_graph.py
@@ -1,8 +1,21 @@
 from collections import defaultdict
 
 
+# helper function to validate the final feature vector
+# based on the list of features provided
+# assumption: all features are numeric
+def _validate_features(row, feature, feature_names):
+    if len(feature) != len(feature_names):
+        return False
+    for idx, feat in enumerate(feature_names):
+        if int(row[feat]) != int(feature[idx]):
+            # print(round(row[feat],1), round(feature[idx],1),type(round(feature[idx],1)), type(round(row[feat],1)))
+            return False
+    return True
+
+
 # validate if customer features in the graph match those in the dataframe
-def validate_customer_features(clean_graph, df):
+def validate_customer_features(clean_graph, df, customer_features):
     cust_features = clean_graph.nodes['customer'].data['features'].tolist()
     cust_ids_from_graph_tensor = clean_graph.nodes('customer')
     cust_ids_from_graph = [id.item() for id in cust_ids_from_graph_tensor]
@@ -17,7 +30,8 @@ def validate_customer_features(clean_graph, df):
     is_mismatch = False
     for index, row in df.iterrows():
         if row['customer_id_int'] in id_feat_map:
-            if id_feat_map[row['customer_id_int']] == [float(row['customer_zip_code_prefix'])]:
+            is_valid = _validate_features(row, id_feat_map[row['customer_id_int']], customer_features)
+            if is_valid:
                 validated_customer_ids.add(row['customer_id_int'])
             else:
                 print(f"mismatch in customer ID: {row['customer_id_int']}")
@@ -33,7 +47,7 @@ def validate_customer_features(clean_graph, df):
 
 
 # validate if product features in the graph match those in the dataframe
-def validate_product_features(clean_graph, df):
+def validate_product_features(clean_graph, df, product_features):
     prod_features = clean_graph.nodes['product'].data['features'].tolist()
     prod_ids_from_graph_tensor = clean_graph.nodes('product')
     prod_ids_from_graph = [id.item() for id in prod_ids_from_graph_tensor]
@@ -48,8 +62,8 @@ def validate_product_features(clean_graph, df):
     is_mismatch = False
     for index, row in df.iterrows():
         if row['product_id_int'] in id_feat_map:
-            graph_feat = id_feat_map[row['product_id_int']]
-            if round(graph_feat[0], 2) == round(row['price'], 2) and graph_feat[1] == float(row['purchase_weekofyear']):
+            is_valid = _validate_features(row, id_feat_map[row['product_id_int']], product_features)
+            if is_valid:
                 validated_product_ids.add(row['product_id_int'])
             else:
                 # please leave these commented lines here to aid future debugging
@@ -107,22 +121,40 @@ def validate_edges(clean_graph, df, etype, src_col_name, dest_col_name):
     return f"Edge assignment for etype {etype} is valid!"
 
 
-# TODO: once we decide edge features, validate edge features
-def validate_edge_features(clean_graph, df, etype, src_col_name, dest_col_name):
+def validate_edge_features(clean_graph, df, etype, edge_features):
+    print(f"----------- validating edge features for {etype} ---------")
     prod_ids_from_graph_tensor = clean_graph.nodes('customer')
     prod_ids_from_graph = [id.item() for id in prod_ids_from_graph_tensor]
     edge_id_dict = {}
     edge_feat_dict = {}
     for node_id in prod_ids_from_graph:
-        output = clean_graph.out_edges(node_id, etype='orders', form='all')
-        print("u", output[0].item())
-        print("v", output[1].item())
-        print("edge ID", output[2].item())
-        u = output[0].item()
-        v = output[1].item()
-        edge_id = output[2].item()
-        edge_feat_dict[(u,v)] = edge_id
+        output = clean_graph.out_edges(node_id, etype=etype, form='all')
+        u = output[0]
+        v = output[1]
+        e = output[2]
+        for i in range(len(e)):
+            edge_id_dict[(u[i].item(), v[i].item())] = e[i].item()
 
-    edge_feats = clean_graph.edges['orders'].data['features'].tolist()
-    for i in edge_feats:
-        edge_feat_dict[i]
+    edge_feats = clean_graph.edges[etype].data['features'].tolist()
+    for i, feat in enumerate(edge_feats):
+        edge_feat_dict[i] = feat
+
+    # edge IDs are assigned row-wise, starting from 0
+    eid = 0
+    validated_eids = set()
+    for index, row in df.iterrows():
+        if (row['customer_id_int'], row['product_id_int']) in edge_id_dict.keys():
+            # eid = edge_id_dict[(row['customer_id_int'], row['product_id_int'])]
+            is_valid = _validate_features(row, edge_feat_dict[eid], edge_features)
+            if is_valid:
+                validated_eids.add(eid)
+            else:
+                print("mismatch")
+        else:
+            print("else")
+            print(row)
+        eid += 1
+
+    if len(validated_eids) == clean_graph.num_edges(etype):
+        return f"validate_edge_features for {etype}: features match!"
+    return f"validate_edge_features for {etype}: features mis-match."
