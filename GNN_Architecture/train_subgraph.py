51f96106005a8cd8bfc9eb301e981e4eb554db24
pratikaher88 <pratikaher88@berkeley.edu>
2023-04-12 12:56:57 -0700

    print model size

--- GNN_Architecture/train_subgraph.py
+++ GNN_Architecture/train_subgraph.py
@@ -4,20 +4,7 @@ import os, random, yaml, time
 from Model.model import ConvModel
 from Model.loss import max_margin_loss, binary_cross_entropy_loss
 from settings import BASE_DIR, CONFIG_PATH
-
-
-def get_model_size(given_model):
-    param_size = 0
-    for param in given_model.parameters():
-        param_size += param.nelement() * param.element_size()
-
-    buffer_size = 0
-    for buffer in given_model.buffers():
-        buffer_size += buffer.nelement() * buffer.element_size()
-
-    size_all_mb = (param_size + buffer_size) / 1024**2
-    print('model size: {:.3f}MB'.format(size_all_mb))
-
+from helper import get_model_size
 
 start = time.time()
 
