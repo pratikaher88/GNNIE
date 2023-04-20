a060a9cd31e1fd972962aacbece87865a109d881
heidi-chiehanlin <chiehan.linn@gmail.com>
2023-04-20 12:04:07 -0700

    Assign features to nodes and edges

---
+++ go_prod/create_and_save_graph.py
@@ -0,0 +1,132 @@
+import dgl
+import torch
+import pandas as pd
+import numpy as np
+# from settings import BASE_DIR
+# from validate_graph import validate_customer_features, validate_product_features, validate_edges
+import uuid
+
+
+BASE_DIR = '' # set this up before running this file
+
+# import the whole set of data features
+df_final = pd.read_csv(BASE_DIR+"/features.csv")
+
+# create list of features that belongs to different type of nodes
+list_of_features = list(df_final.columns)
+
+list_of_user_features = [f for f in list_of_features if "id" not in f and "user" in f]
+len(list_of_user_features), list_of_user_features
+
+list_of_product_features = [f for f in list_of_features if "id" not in f and "product" in f]
+len(list_of_product_features), list_of_product_features
+
+list_of_order_features = [f for f in list_of_features if "id" not in f and "_order" in f]
+len(list_of_order_features), list_of_order_features
+
+list_of_revorder_features = [f for f in list_of_features if "id" not in f and "_revorder" in f]
+len(list_of_revorder_features), list_of_revorder_features
+# sanity check
+# set(list_of_features) - set(list_of_user_features) - set(list_of_product_features) - set(list_of_order_features) - set(list_of_revorder_features)
+
+
+### generate integer node ids ###
+
+# Create a dictionary that maps UUIDs to integer IDs
+uuid_to_int = {}
+for i, u in enumerate(df_final['customer_id'].unique()):
+    uuid_to_int[u] = i
+
+# Map the UUIDs to integer IDs using the dictionary
+df_final['customer_id_int'] = df_final['customer_id'].map(uuid_to_int)
+
+# repaet the process for product id
+uuid_to_int = {}
+for i, u in enumerate(df_final['product_id'].unique()):
+    uuid_to_int[u] = i
+
+df_final['product_id_int'] = df_final['product_id'].map(uuid_to_int)
+
+
+
+### generate integer edge ids ###
+
+# in the graph, order ids should be the indicator of "pairs between user and product id"
+df_final['new_order_id'] = df_final.customer_id.str.cat(df_final.product_id)
+# concatenate user and prod node ids so that df_final.new_order_id.nunique() matches ecommerce_hetero_graph.num_edges
+# df_final.new_order_id.nunique()
+
+# repaet the process for node id
+uuid_to_int = {}
+for i, u in enumerate(df_final['new_order_id'].unique()):
+    uuid_to_int[u] = i
+
+df_final['order_id_int'] = df_final['new_order_id'].map(uuid_to_int)
+
+
+### create graph ###
+
+graph_data = {
+        ('customer','orders','product') : (df_final['customer_id_int'].to_numpy(), df_final['product_id_int'].to_numpy()),
+        ('product','rev-orders','customer') : (df_final['product_id_int'].to_numpy(), df_final['customer_id_int'].to_numpy())
+    }
+ecommerce_hetero_graph = dgl.heterograph(graph_data)
+
+
+### node feature assignment ###
+
+def _process_customer_features(df_final):
+    HM = {}
+    for _, row in df_final.iterrows():
+        HM[row['customer_id_int']] = torch.tensor([row[list_of_user_features]]).float()
+    return HM
+
+custid_to_feat = _process_customer_features(df_final)
+customer_features = [value[1] for value in list(custid_to_feat.items())]
+ecommerce_hetero_graph.nodes['customer'].data['features'] = torch.stack(customer_features, axis=0)
+
+
+def _process_product_features(df_final):
+    HM = {}
+    for _, row in df_final.iterrows():
+        HM[row['product_id_int']] = torch.tensor([row[list_of_product_features]])
+    return HM
+prodid_to_feat = _process_product_features(df_final)
+product_features = [value[1] for value in list(prodid_to_feat.items())]
+ecommerce_hetero_graph.nodes['product'].data['features'] = torch.stack(product_features, axis=0)
+
+
+### edge feature assignment ###
+
+def _process_order_features(df_final):
+    HM = {}
+    for _, row in df_final.iterrows():
+        HM[row['order_id_int']] = torch.tensor([row[list_of_order_features]])
+    return HM
+
+orderid_to_feat = _process_order_features(df_final)
+order_features = [value[1] for value in list(orderid_to_feat.items())]
+ecommerce_hetero_graph.edges['orders'].data['features'] = torch.stack(order_features, axis=0)
+
+
+def _process_revorder_features(df_final):
+    HM = {}
+    for _, row in df_final.iterrows():
+        HM[row['order_id_int']] = torch.tensor([row[list_of_revorder_features]])
+    return HM
+
+orderid_to_feat = _process_revorder_features(df_final)
+revorder_features = [value[1] for value in list(orderid_to_feat.items())]
+ecommerce_hetero_graph.edges['rev-orders'].data['features'] = torch.stack(revorder_features, axis=0)
+
+### graph validation is skipped for now because it's not generalized yet ###
+
+# # run validation scripts
+# # TODO: error handling in case validation scripts return errors
+# print(validate_customer_features(ecommerce_hetero_graph, df_final))
+# print(validate_product_features(ecommerce_hetero_graph, df_final))
+# print(validate_edges(ecommerce_hetero_graph, df_final, 'orders', 'customer_id_int', 'product_id_int'))
+# print(validate_edges(ecommerce_hetero_graph, df_final, 'rev-orders', 'product_id_int', 'customer_id_int'))
+
+print("SAVE GRAPH !!")
+dgl.save_graphs(f"{BASE_DIR}/graph_w_features/ecommerce_hetero_graph.dgl", [ecommerce_hetero_graph])
--- go_prod/feature_engineering.py
+++ go_prod/feature_engineering.py
@@ -16,8 +16,8 @@ from datetime import timedelta
 
 
 # CONFIGS
-UNIFIED_CSV_PATH = '/Users/benjaminfell/Documents/GitHub/GNNIE/DatasetEDA/archive/unified.csv'
-BASE_DIR = '/Users/benjaminfell/Documents/GitHub/GNNIE/go_prod'
+UNIFIED_CSV_PATH = 'DatasetEDA/archive/unified.csv'
+BASE_DIR = 'go_prod'
 
 USER_NODE = "customer_id"
 PRODUCT_NODE = "product_id"
@@ -32,7 +32,7 @@ USER_FEATURES = [
     #"gnn_user_zip_code_2",
     "gnn_user_zip_code_1",
 ]
-PRODUCT_FEATURES = [    
+PRODUCT_FEATURES = [
     "gnn_product_reviews_average",
     "gnn_product_reviews_last",
     "gnn_product_reviews_last3_average",
@@ -47,7 +47,7 @@ PRODUCT_FEATURES = [
     "gnn_product_quantity",
 ]
 ORDER_EDGE = "order_id"
-ORDER_FEATURES = [ 
+ORDER_FEATURES = [
     "gnn_order_items_count",
     "gnn_order_time_purchased_to_approved",
     "gnn_order_time_approved_to_carrier",
@@ -85,33 +85,33 @@ REV_ORDER_FEATURES = [
 ]
 
 RAW_COLUMNS_REQUIREMENTS = [
-    ORDER_EDGE, 
-    USER_NODE, 
+    ORDER_EDGE,
+    USER_NODE,
     PRODUCT_NODE,
     'order_item_id',  #Remove when derived df function is partially moved to merge_data.py
     'seller_id',
-    'shipping_limit_date', 
-    'price', 
-    'freight_value', 
+    'shipping_limit_date',
+    'price',
+    'freight_value',
     'product_name_lenght',
-    'product_description_lenght', 
-    'product_photos_qty', 
+    'product_description_lenght',
+    'product_photos_qty',
     'product_weight_g',
-    'product_length_cm', 
-    'product_height_cm', 
+    'product_length_cm',
+    'product_height_cm',
     'product_width_cm',
-    'product_category_name_english', 
+    'product_category_name_english',
     'review_score',
-    'review_comment_message', 
-    'customer_id', 
+    'review_comment_message',
+    'customer_id',
     'order_status',
-    'order_purchase_timestamp', 
+    'order_purchase_timestamp',
     'order_approved_at',
     'order_delivered_carrier_date',
     'order_delivered_customer_date',
-    'order_estimated_delivery_date', 
-    'customer_zip_code_prefix', 
-    'customer_city', 
+    'order_estimated_delivery_date',
+    'customer_zip_code_prefix',
+    'customer_city',
     'customer_state',
 ]
 
@@ -124,7 +124,7 @@ def check_columns(df):
     Check if all the specified columns exist in the data. Return dataframe that contains only the selected columns.
     :return: boolean
     """
-    
+
     print("Checking Columns...")
     exist_columns = df.columns.tolist()
 
@@ -144,7 +144,7 @@ def remove_duplicate(df):
     Remove deplicate records. Report duplicate percentage when it's higher than 20%.
     :return: deduped dataframe
     """
-    
+
     print("Removing Duplicates...")
 
     N = df.shape[0]
@@ -170,7 +170,7 @@ def handle_nulls(df):
     * Categorical features: (1) drop the column if null_pct >=5%. (2) remove the record if null_pct <5%.
     :return: dataframe
     """
-    
+
     print("Handling Nulls...")
 
     df.dropna(subset=[USER_NODE, PRODUCT_NODE], inplace=True)
@@ -239,7 +239,7 @@ def one_hot_encode_and_bind(original_dataframe, feature_to_encode):
     dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
     res = pd.concat([original_dataframe, dummies], axis=1)
     res = res.drop([feature_to_encode], axis=1)
-    return(res) 
+    return(res)
 
 
 # In[9]:
@@ -251,26 +251,26 @@ def derive_dataframes(df):
     datetime_cols = ['order_purchase_timestamp','order_delivered_carrier_date','order_approved_at','order_delivered_customer_date','order_estimated_delivery_date','shipping_limit_date']
     df[datetime_cols] = df[datetime_cols].astype(str)
     df[datetime_cols] = df[datetime_cols].apply(pd.to_datetime)
-    
+
     df = df.sort_values(by=['order_purchase_timestamp'], ignore_index = True)
-    
+
     df_order_items = df.groupby(df.columns.difference(['order_item_id','review_score']).tolist(),as_index=False).agg({
                          'order_item_id': 'count',
                          'review_score': 'mean'}).rename(columns={'order_item_id': 'quantity'})
-    
+
     assert len(df_order_items.groupby(['order_id','product_id']).filter(lambda x: len(x) > 1)) == 0 , "Dataset does not contain one row per order-item"
-    
+
     df_orders = df_order_items.groupby(df_order_items.columns.difference(['price','freight_value','product_weight_g','review_score','quantity','product_category_name_english','product_description_lenght','product_name_lenght','product_height_cm','product_length_cm','product_width_cm','seller_id','shipping_limit_date','customer_unique_id','product_id','product_photos_qty']).tolist(),as_index=False).agg({
-                         'price': 'sum', 
-                         'freight_value':'sum', 
-                         'product_weight_g':'sum', 
+                         'price': 'sum',
+                         'freight_value':'sum',
+                         'product_weight_g':'sum',
                          'review_score':'mean',
                          'quantity': 'sum'})
-    
+
     assert len(df_orders.groupby("order_id").filter(lambda x: len(x) > 1)) == 0 , "Derived order dataset does not contain one row per order"
-    
+
     return df_order_items, df_orders
-    
+
 
 
 # In[22]:
@@ -284,27 +284,27 @@ def feature_engineering(df):
     print("Extracting Features...")
 
     df_order_items, df_orders = derive_dataframes(df)
-    
+
 ##### USER_FEATURES
     print("Extracting USER Features...")
 
     if "gnn_user_customer_state" in USER_FEATURES:
         df_orders.rename(columns={'customer_state': 'gnn_user_customer_state'},inplace=True)
         df_orders = one_hot_encode_and_bind(df_orders,"gnn_user_customer_state")
-      
+
     if "gnn_user_reviews_average" in USER_FEATURES:
         df_orders['gnn_user_reviews_average'] = df_orders.groupby('customer_id')['review_score'].transform('mean')
-     
+
     if "gnn_user_reviews_last" in USER_FEATURES:
         df_orders['gnn_user_reviews_last'] = df_orders.groupby('customer_id')['review_score'].transform('last')
-    
+
     if "gnn_user_reviews_last3_average" in USER_FEATURES:
         df_orders['gnn_user_reviews_last3_average'] = df_orders.groupby('customer_id')['review_score'].transform(lambda x: x.rolling(3, 1).mean())
 
     if "gnn_user_zip_code_5" in USER_FEATURES:
         df_orders['gnn_user_zip_code_5'] = df_orders['customer_zip_code_prefix']
         df_orders = one_hot_encode_and_bind(df_orders,"gnn_user_zip_code_5")
-   
+
     if "gnn_user_zip_code_4" in USER_FEATURES:
         df_orders['gnn_user_zip_code_4'] = df_orders['customer_zip_code_prefix'].map(lambda x: str(x)[:-1])
         df_orders = one_hot_encode_and_bind(df_orders,"gnn_user_zip_code_4")
@@ -312,7 +312,7 @@ def feature_engineering(df):
     if "gnn_user_zip_code_3" in USER_FEATURES:
         df_orders['gnn_user_zip_code_3'] = df_orders['customer_zip_code_prefix'].map(lambda x: str(x)[:-2])
         df_orders = one_hot_encode_and_bind(df_orders,"gnn_user_zip_code_3")
-    
+
     if "gnn_user_zip_code_2" in USER_FEATURES:
         df_orders['gnn_user_zip_code_2'] = df_orders['customer_zip_code_prefix'].map(lambda x: str(x)[:-3])
         df_orders = one_hot_encode_and_bind(df_orders,"gnn_user_zip_code_2")
@@ -321,25 +321,25 @@ def feature_engineering(df):
         df_orders['gnn_user_zip_code_1'] = df_orders['customer_zip_code_prefix'].map(lambda x: str(x)[:-4])
         df_orders = one_hot_encode_and_bind(df_orders,"gnn_user_zip_code_1")
 
-        
+
 ##### PRODUCT_FEATURES
     print("Extracting PRODUCT Features...")
 
     if "gnn_product_reviews_average" in PRODUCT_FEATURES:
         df_order_items['gnn_product_reviews_last'] = df_order_items.groupby('product_id')['review_score'].transform('mean')
-    
+
     if "gnn_product_reviews_last" in PRODUCT_FEATURES:
         df_order_items['gnn_product_reviews_last'] = df_order_items.groupby('product_id')['review_score'].transform('last')
-     
+
     if "gnn_product_reviews_last3_average" in PRODUCT_FEATURES:
         df_order_items['gnn_product_reviews_last3_average'] = df_order_items.groupby('product_id')['review_score'].transform(lambda x: x.rolling(3, 1).mean())
-        
+
     if "gnn_product_seller_count" in PRODUCT_FEATURES:
         df_order_items['gnn_product_seller_count'] = df_order_items.groupby('product_id')['seller_id'].transform('nunique')
-       
+
     if "gnn_product_avg_price" in PRODUCT_FEATURES:
         df_order_items['gnn_product_avg_price'] = df_order_items.groupby('product_id')['price'].transform('mean')
-    
+
     if "gnn_product_volume_cm3" in PRODUCT_FEATURES:
         df_order_items['gnn_product_volume_cm3'] = df_order_items['product_height_cm']*df_order_items['product_length_cm']*df_order_items['product_width_cm']
 
@@ -348,7 +348,7 @@ def feature_engineering(df):
 
     if "gnn_product_description_lenght" in PRODUCT_FEATURES:
         df_order_items.rename(columns={'product_description_lenght': 'gnn_product_description_lenght'},inplace=True)
-    
+
     if "gnn_product_category_code" in PRODUCT_FEATURES:
         df_order_items.rename(columns={'product_category_name_english': 'gnn_product_category_code'},inplace=True)
         df_order_items = one_hot_encode_and_bind(df_order_items,"gnn_product_category_code")
@@ -358,7 +358,7 @@ def feature_engineering(df):
 
     if "gnn_product_weight_g" in PRODUCT_FEATURES:
         df_order_items.rename(columns={'product_weight_g': 'gnn_product_weight_g'},inplace=True)
- 
+
     if "gnn_product_quantity" in PRODUCT_FEATURES:
             df_order_items.rename(columns={'quantity': 'gnn_product_quantity'},inplace=True)
 
@@ -368,23 +368,23 @@ def feature_engineering(df):
 
     if "gnn_order_items_count" in ORDER_FEATURES:
         df_orders.rename(columns={"quantity": "gnn_order_items_count"},inplace=True)
-    
+
     if "gnn_order_time_purchased_to_approved" in ORDER_FEATURES:
         df_orders['gnn_order_time_purchased_to_approved'] = df_orders['order_approved_at'] - df_orders['order_purchase_timestamp']
         df_orders['gnn_order_time_purchased_to_approved'] = df_orders['gnn_order_time_purchased_to_approved'].dt.total_seconds()/60
-    
+
     if "gnn_order_time_approved_to_carrier" in ORDER_FEATURES:
         df_orders['gnn_order_time_approved_to_carrier'] = df_orders['order_delivered_carrier_date'] - df_orders['order_approved_at']
         df_orders['gnn_order_time_approved_to_carrier'] = df_orders['gnn_order_time_approved_to_carrier'].dt.total_seconds()/60
-    
+
     if "gnn_order_time_carrier_to_customer" in ORDER_FEATURES:
         df_orders['gnn_order_time_carrier_to_customer'] = df_orders['order_delivered_customer_date'] - df_orders['order_delivered_carrier_date']
         df_orders['gnn_order_time_carrier_to_customer'] = df_orders['gnn_order_time_carrier_to_customer'].dt.total_seconds()/60
-          
+
     if "gnn_order_time_customer_to_eta" in ORDER_FEATURES:
         df_orders['gnn_order_time_customer_to_eta'] = df_orders['order_estimated_delivery_date'] + timedelta(days=1) - df_orders['order_delivered_customer_date']
         df_orders['gnn_order_time_customer_to_eta'] = df_orders['gnn_order_time_customer_to_eta'].dt.total_seconds()/60
-        
+
     if "gnn_order_is_on_time_delivery" in ORDER_FEATURES:
         df_orders['gnn_order_is_on_time_delivery'] = np.where(df_orders['gnn_order_time_customer_to_eta'] >= 0, 1, 0)
 
@@ -393,20 +393,20 @@ def feature_engineering(df):
 
     if "gnn_order_freight_value" in ORDER_FEATURES:
         df_orders.rename(columns={'freight_value': 'gnn_order_freight_value'},inplace=True)
-    
+
     if "gnn_order_weight_g" in ORDER_FEATURES:
         df_orders.rename(columns={'product_weight_g': 'gnn_order_weight_g'},inplace=True)
-    
+
     if "gnn_order_item_count" in ORDER_FEATURES:
         df_orders.rename(columns={'quantity': 'gnn_order_item_count'},inplace=True)
 
-    
+
 ##### REV_ORDER_FEATURES
     print("Extracting REV-ORDER Features...")
-   
+
     if "gnn_revorder_review_score" in REV_ORDER_FEATURES:
         df_orders.rename(columns={'review_score': 'gnn_revorder_review_score'},inplace=True)
-        
+
 ##### MERGE & CLEAN DATAFRAMES
     print("Cleaning & Merging Features...")
 
@@ -420,9 +420,9 @@ def feature_engineering(df):
 
     #Merge dataframes
     df_features = pd.merge(df_order_items, df_orders, on=[USER_NODE, ORDER_EDGE])
-    
-    
-    
+
+
+
     #Validate featured df size before returning
     assert len(df_features) == len(df_order_items), f"Features dataframe row size ({len(features_df)}) does not match original row size ({len(df_order_items)})"
 
