93b361761d952e222d6daf427d99c79f01ac7a22
pratikaher88 <pratikaher88@berkeley.edu>
2023-04-28 09:23:39 -0700

    master script changes

--- GNN_Architecture/generate_graph/master_create_graph.py
+++ GNN_Architecture/generate_graph/master_create_graph.py
@@ -10,10 +10,10 @@ import subprocess
 
 # 2. create graphs without only two features
 # do not remove duplicates
-# subprocess.run(["python3", "generate_graph/create_and_save_graph.py"])
+subprocess.run(["python3", "generate_graph/create_and_save_graph.py"])
 # remove duplicates
 # subprocess.run(["python3", "generate_graph/create_and_save_graph.py", '--remove_duplicates'])
 
 # 3. add structural features to graph
-subprocess.run(["python3", "generate_graph/add_structural_features.py", '--graph_name', "ecommerce_hetero_graph" ])
+# subprocess.run(["python3", "generate_graph/add_structural_features.py", '--graph_name', "ecommerce_hetero_graph" ])
 
