daf1041e733bb2e6e54d97dabc408d04d216ef36
pratikaher88 <pratikaher88@berkeley.edu>
2023-06-25 16:48:19 -0700

    push changes

--- GNN_Architecture/train_subgraph.py
+++ GNN_Architecture/train_subgraph.py
@@ -127,9 +127,9 @@ for i in range(model_config['n_epochs']):
         for key, value in edge_features.items():
             HM[key[1]] = (value, )
         
-        print("Edge Features shape : ", HM['orders'][0].shape, HM['rev-orders'][0].shape)
-        print(input_features['customer'].shape, input_features['product'].shape)
-        print(len(blocks))
+        # print("Edge Features shape : ", HM['orders'][0].shape, HM['rev-orders'][0].shape)
+        # print(input_features['customer'].shape, input_features['product'].shape)
+        # print(len(blocks))
 
         _, pos_score, neg_score = model(blocks, input_features, HM, pos_g, neg_g)
         # _, pos_score, neg_score = model(blocks, input_features, edge_features, pos_g, neg_g)
