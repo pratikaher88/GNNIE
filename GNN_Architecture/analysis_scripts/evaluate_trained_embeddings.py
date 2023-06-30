9f0fbc42e8e8a118b8c0c2631cacbab455f3cfbd
pratikaher88 <pratikaher88@berkeley.edu>
2023-06-29 18:55:16 -0700

    latest changes

--- GNN_Architecture/analysis_scripts/evaluate_trained_embeddings.py
+++ GNN_Architecture/analysis_scripts/evaluate_trained_embeddings.py
@@ -81,7 +81,7 @@ def get_model_recs():
 model_recommendations = get_model_recs()
 
 print("Model recs length",len(model_recommendations))
-print("Sample model rec", model_recommendations[:10])
+print("Sample model rec", model_recommendations)
 
 # print("Valid graph length",len(recommendations_from_valid_graph))
 
